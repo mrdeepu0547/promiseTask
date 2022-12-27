b52fa504518e36ce5eca8ab87717f9a93ecc9aca
mrdeepu0547 <saideepak.sabbineni2000@gmail.com>
2022-12-27 10:23:18 +0530

    code to send an HTTP GET request and process the response using a Promise.
    ADVERTISEMENT

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'promiseTask';
+  title = "Angular 10 and Promises Example";
+
+  API_KEY = "e40d07f00b094602953cc3bf8537477e";
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    console.log("Angular 10 Promises");
+    this.fetchDataAsPromise()
+      .then((data) => {
+        console.log(JSON.stringify(data));
+      })
+      .catch((error) => {
+        console.log("Promise rejected with " + JSON.stringify(error));
+      });
+  }
+
+  fetchDataAsPromise() {
+    return this.httpClient
+    .get(
+        `https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=${this.API_KEY}`
+      )
+      .toPromise();
+  }
 }
